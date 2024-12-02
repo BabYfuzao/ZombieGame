9341ce5d5d9d678e5b64f3efd816fedee80fcf9b
Riwu <riwu1219@gmail.com>
2024-12-02 11:38:30 +0800

    HpBar template done

--- Assets/Material/test_riwu/HpBar.cs
+++ Assets/Material/test_riwu/HpBar.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class HP_Bar : MonoBehaviour
 {
-    GameObject HpBar;
+    public GameObject HpBar;
+    public float maxHP;
+    public float HP;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        HP = maxHP;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float p;
+        p = HP/maxHP; 
+
+        if (HP >= 0){
+            HpBar.transform.localScale = new Vector3(1*p, 1, 0);
+        }else if (HP < 0){
+            HpBar.transform.localScale = new Vector3(0, 1, 0);
+        }else if (p >= 1){
+            HpBar.transform.localScale = new Vector3(1, 1, 0);
+        }
     }
 }
